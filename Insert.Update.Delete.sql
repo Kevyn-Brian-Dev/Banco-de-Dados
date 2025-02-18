dbde353492632287cbbf436dd3d6aca87b29d885
Kevyn-Brian-Dev <kevynbrian9@gmail.com>
2025-02-17 21:10:57 -0300

    Update Insert.Update.Delete.sql

--- Insert.Update.Delete.sql
+++ Insert.Update.Delete.sql
@@ -2,5 +2,6 @@
 select * from clientes
 
 update clientes set nome = 'Iza', rg = '4.378.082', data_nascimento ='11-04-2000', cel_cliente = '9233-4510', email='iza.belly@gmail.com',endereco_cliente = 'Rua Do Matadouro'
+where condicao;
 
 delete from clientes where clientes.id_cliente = 1;
